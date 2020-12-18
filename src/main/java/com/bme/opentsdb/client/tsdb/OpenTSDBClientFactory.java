cface26fee71e2e45912d345cd62c3243d723b6c
bme_yutao <2686222074@qq.com>
2020-12-18 18:55:15 +0800

    OpenTSDBClientFactory构建OpenTSDBClient优化

--- src/main/java/com/bme/opentsdb/client/tsdb/OpenTSDBClientFactory.java
+++ src/main/java/com/bme/opentsdb/client/tsdb/OpenTSDBClientFactory.java
@@ -1,16 +1,11 @@
 package com.bme.opentsdb.client.tsdb;
 
-import com.bme.opentsdb.client.bean.request.Point;
-import com.bme.opentsdb.client.bean.response.DetailResult;
-import com.bme.opentsdb.client.http.callback.BatchPutHttpResponseCallback;
 import lombok.Data;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.nio.reactor.IOReactorException;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Objects;
 
 /**
@@ -27,10 +22,17 @@ public class OpenTSDBClientFactory {
     private String host;
     private Integer port;
 
+    public OpenTSDBClient build() throws IOReactorException {
+        return this.build(null);
+    }
+
     public OpenTSDBClient build(OpenTSDBConfig config) throws IOReactorException {
+        if (config == null) {
+            config = OpenTSDBConfig.address().config();
+        }
         if (StringUtils.isEmpty(config.getHost())) {
-            Objects.requireNonNull(host,"host");
-            Objects.requireNonNull(port,"port");
+            Objects.requireNonNull(host, "host");
+            Objects.requireNonNull(port, "port");
             config.setHost(host);
             config.setPort(port);
         }
